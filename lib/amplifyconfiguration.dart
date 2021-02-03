70da245808a45271f5405b3f992073be6ffe0a5b
jelavallee <jelavallee@gmail.com>
2021-02-02 23:41:18 -0500

    Ran 'amplify add api' and 'amplify push'; Initial generated config/schema

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,4 +1,17 @@
 const amplifyconfig = ''' {
     "UserAgent": "aws-amplify-cli/2.0",
-    "Version": "1.0"
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "todofluttergqldemo": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://gbf7oclmjve4xe3pq5bxkrwfkm.appsync-api.us-east-1.amazonaws.com/graphql",
+                    "region": "us-east-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-l7nsuogjhzh4hgbsmqhg5fxq5m"
+                }
+            }
+        }
+    }
 }''';
