0f63d6238d5fc1e43c9efc7679fc8d78c3144dea
jelavallee <jelavallee@gmail.com>
2021-02-03 02:48:19 -0500

    Wired in very clunky GQL Query handler to retrieve Todo items. Woohoo!

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify.dart';
 import 'package:amplify_api/amplify_api.dart';
@@ -36,20 +38,30 @@ class TodoWidget extends StatefulWidget {
 }
 
 class TodoListState extends State<TodoWidget> {
+  // Amplify service
+  final _amplify = Amplify;
+  bool _amplifyConfigured = false;
+
   // Placeholder state entities
-  List<Todo> todoItems = [
-    Todo("12345", "Learn AWS Amplify...", false),
-    Todo("23456", "Learn AWS AppSync...", false),
-    Todo("34678", "Make Serverless GraphQL happen!", false),
-  ];
+  List<Todo> todoItems = [];
 
   @override
   void initState() {
     super.initState();
 
     // Initialize Amplify service components
-    Amplify.addPlugin(AmplifyAPI());
-    Amplify.configure(amplifyconfig);
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    _amplify.addPlugin(AmplifyAPI());
+    try {
+      await _amplify.configure(amplifyconfig);
+      print('Successfully configured Amplify!!!');
+      doLoadTodoList();
+    } catch (e) {
+      print('Could not configure Amplify...');
+    }
   }
 
   // Callbacks
@@ -66,6 +78,39 @@ class TodoListState extends State<TodoWidget> {
       todoItems
           .add(Todo(randomNumber.nextInt(100000).toString(), name, completed));
     });
+  }
+
+  // Service Handlers
+  void doLoadTodoList() async {
+    try {
+      String gqlQuery = '''query{
+        listTodos{
+          items {
+            id
+            name
+            completed
+          }
+        }
+      }''';
+
+      var operation = Amplify.API
+          .query(request: GraphQLRequest<String>(document: gqlQuery));
+
+      var response = await operation.response;
+      var data = response.data;
+      Map<String, dynamic> listTodos = jsonDecode(data);
+      List<dynamic> todoResponseItems = listTodos['listTodos']['items'];
+      List<Todo> todoItemsList = [];
+      todoResponseItems.forEach((item) {
+        print('Adding Item: ' + item.toString());
+        todoItemsList.add(Todo(item['id'], item['name'], item['completed']));
+      });
+      setState(() {
+        todoItems = todoItemsList;
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
