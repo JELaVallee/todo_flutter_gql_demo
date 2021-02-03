50ac6ad4313d5ee1e791dbbe15c7060e56a4577d
jelavallee <jelavallee@gmail.com>
2021-02-03 01:13:09 -0500

    Added ListTodosView and AddTodoView widets, route handling and callbacks

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import 'amplifyconfiguration.dart';
 // Models
 import './model/todo.dart';
 
+import 'dart:math';
+
+Random randomNumber = new Random();
+
 void main() {
   runApp(TodoApp());
 }
@@ -43,9 +47,17 @@ class TodoListState extends State<TodoWidget> {
   }
 
   // Callbacks
+  // Switch completion state for toggled todo item
   void onTodoToggle(Todo todo) {
     setState(() {
       todo.setCompleted(!todo.isCompleted());
+    });
+  }
+
+  // Create new todo item
+  void onTodoAdd(String id, String name, bool completed) {
+    setState(() {
+      todoItems.add(Todo(id, name, completed));
     });
   }
 
@@ -57,18 +69,77 @@ class TodoListState extends State<TodoWidget> {
           primarySwatch: Colors.amber,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Todo App'),
-            ),
-            body: ListView.builder(
-                itemCount: todoItems.length,
-                itemBuilder: (context, index) {
-                  return CheckboxListTile(
-                    title: Text(todoItems[index].getName()),
-                    value: todoItems[index].isCompleted(),
-                    onChanged: (_) => onTodoToggle(todoItems[index]),
-                  );
-                })));
+        initialRoute: "/",
+        routes: {
+          '/': (context) =>
+              ListTodosView(todoItems: todoItems, onTodoToggle: onTodoToggle),
+          '/addtodo': (context) => AddTodoView(onTodoAdd: onTodoAdd)
+        });
+  }
+}
+
+class ListTodosView extends StatelessWidget {
+  final List<Todo> todoItems;
+  final onTodoToggle;
+
+  ListTodosView({@required this.todoItems, @required this.onTodoToggle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo App'),
+      ),
+      body: ListView.builder(
+          itemCount: todoItems.length,
+          itemBuilder: (context, index) {
+            return CheckboxListTile(
+              title: Text(todoItems[index].getName()),
+              value: todoItems[index].isCompleted(),
+              onChanged: (_) => onTodoToggle(todoItems[index]),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.pushNamed(context, '/addtodo'),
+          child: Icon(Icons.add)),
+    );
+  }
+}
+
+class AddTodoView extends StatefulWidget {
+  final onTodoAdd;
+
+  AddTodoView({@required this.onTodoAdd});
+
+  @override
+  State<StatefulWidget> createState() {
+    return AddTodoState();
+  }
+}
+
+class AddTodoState extends State<AddTodoView> {
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('New Todo Item')),
+      body: Center(
+          child: Padding(
+              padding: EdgeInsets.all(16),
+              child: TextField(
+                  autofocus: true,
+                  controller: textController,
+                  decoration: InputDecoration(
+                      labelText: 'Enter your new todo item...')))),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () {
+          widget.onTodoAdd(randomNumber.nextInt(100000).toString(),
+              textController.text, false);
+          Navigator.pop(context);
+        },
+      ),
+    );
   }
 }
